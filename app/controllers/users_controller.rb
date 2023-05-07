409160d2983f5fe2a6f33ac6d7c3c32d2465fd5e
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-05-07 14:47:18 +0000

    [Add]課題3　User画面作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
